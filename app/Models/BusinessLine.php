44eed168a2c80c22e148128e23e8d8bb9751e4a9
Miguel <tecnologia1@zcmayoristas.com>
2024-11-22 18:02:02 -0500

    Ajuste del modelo busines line

--- app/Models/BusinessLine.php
+++ app/Models/BusinessLine.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BusinessLine extends Model
 {
@@ -14,4 +16,28 @@ class BusinessLine extends Model
      * @var string
      */
     protected $table = 'business_lines';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'image',
+        'image_url', 'status'
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    /**
+     * Relación con las marcas (many-to-many).
+     */
+    public function brands()
+    {
+        return $this->belongsToMany(Brand::class, 'business_line_brand', 'business_line_id', 'brand_id');
+    }
+
 }
