24fec45f35a7b1a1d60a82f9c42765167f79f507
Miguel <tecnologia1@zcmayoristas.com>
2024-11-22 11:01:08 -0500

    creacion de nuevos campos en la tabla brands, para imagenes

--- database/migrations/2024_10_04_155649_create_brands_table.php
+++ database/migrations/2024_10_04_155649_create_brands_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('slug')->unique();
             $table->text('description')->nullable();
-            $table->string('image')->nullable();
-            $table->string('image_url')->nullable();
+            $table->string('image_light')->nullable();
+            $table->string('image_light_url')->nullable();
+            $table->string('image_dark')->nullable();
+            $table->string('image_dark_url')->nullable();
             $table->enum('status', ['active', 'inactive']);
             $table->timestamps();
         });
