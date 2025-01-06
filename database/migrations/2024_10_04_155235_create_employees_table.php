7114c0900cbc68e299163e17b0ed232e939fee4b
Miguel <tecnologia1@zcmayoristas.com>
2025-01-06 12:54:24 -0500

    Agregar el campo uuid para el modelo employee

--- database/migrations/2024_10_04_155235_create_employees_table.php
+++ database/migrations/2024_10_04_155235_create_employees_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->nullable()->unique();
             $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('department_id')->nullable()->constrained('departments')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('position_id')->nullable()->constrained('positions')->onUpdate('cascade')->onDelete('set null');
