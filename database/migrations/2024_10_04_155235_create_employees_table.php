ea623ddaa982eb5336b4981157321c6bd5afde2c
Miguel <tecnologia1@zcmayoristas.com>
2025-02-05 17:51:51 -0500

    NUEVO CAMPO EN EMPLOYEES current_description

--- database/migrations/2024_10_04_155235_create_employees_table.php
+++ database/migrations/2024_10_04_155235_create_employees_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
             $table->enum('gender', ['male', 'female'])->nullable();
             $table->date('birthdate');
             $table->unsignedTinyInteger('age');
-            $table->date('hire_date');  // Fecha de contratacion
+            $table->date('hire_date');  // Fecha de contratacion o inicio
             $table->string('address');
             $table->string('phone_one');
             $table->string('phone_two')->nullable();
@@ -41,6 +41,7 @@ return new class extends Migration
             $table->foreignId('city_id')->constrained('cities')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('state_id')->constrained('states')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('country_id')->constrained('countries')->onUpdate('cascade')->onDelete('cascade');
+            $table->text('current_description ')->nullable();
             $table->timestamps();
         });
     }
